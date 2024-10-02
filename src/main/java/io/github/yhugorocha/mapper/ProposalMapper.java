0cc00656164bfc08e9361c3c4a9ff0c44bd4ba1f
Hugo Silva <jose.silva@extreme.digital>
2024-10-02 09:30:06 -0300

    feat: add class ProposalMapper

---
+++ src/main/java/io/github/yhugorocha/mapper/ProposalMapper.java
@@ -0,0 +1,36 @@
+package io.github.yhugorocha.mapper;
+
+import io.github.yhugorocha.dto.ProposalDetailDTO;
+import io.github.yhugorocha.dto.ProposalRequestDTO;
+import io.github.yhugorocha.entity.ProposalEntity;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.time.LocalDateTime;
+
+@ApplicationScoped
+public class ProposalMapper {
+
+    public ProposalDetailDTO toProposalDetailDTO(ProposalEntity proposalEntity){
+        return ProposalDetailDTO.builder()
+                .id(proposalEntity.getId())
+                .customer(proposalEntity.getCustomer())
+                .priceTonne(proposalEntity.getPriceTonne())
+                .tonnes(proposalEntity.getTonnes())
+                .country(proposalEntity.getCountry())
+                .proposalValidityDays(proposalEntity.getProposalValidityDays())
+                .created(proposalEntity.getCreated())
+                .build();
+    }
+
+    public ProposalEntity toProposalEntity(ProposalRequestDTO proposalRequestDTO){
+        return ProposalEntity.builder()
+                .customer(proposalRequestDTO.costumer())
+                .priceTonne(proposalRequestDTO.priceTonne())
+                .tonnes(proposalRequestDTO.tonnes())
+                .country(proposalRequestDTO.country())
+                .proposalValidityDays(proposalRequestDTO.proposalValidityDays())
+                .created(LocalDateTime.now())
+                .build();
+    }
+
+}
