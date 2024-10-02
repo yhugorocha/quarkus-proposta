545136c4fa4c96079683709734d903963d6c546b
Hugo Silva <jose.silva@extreme.digital>
2024-10-02 09:29:02 -0300

    feat: add class KafkaEvent

---
+++ src/main/java/io/github/yhugorocha/message/KafkaEvent.java
@@ -0,0 +1,23 @@
+package io.github.yhugorocha.message;
+
+import io.github.yhugorocha.dto.ProposalRequestDTO;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+public class KafkaEvent {
+
+    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+    @Channel("proposal")
+    Emitter<ProposalRequestDTO> proposalRequestEmitter;
+
+    public void sendNewKafkaEvent(ProposalRequestDTO proposalRequestDTO){
+        LOG.info("-- Enviando nova proposta para topico kafka --");
+        proposalRequestEmitter.send(proposalRequestDTO).toCompletableFuture().join();
+    }
+
+}
